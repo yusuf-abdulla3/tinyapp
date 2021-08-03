b64745231f46785f27003b2fcd01567b3f74a84e
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-08-03 04:15:45 +0000

    added new endpoint for the shortURL and created a new ejs file called urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -27,6 +27,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: "longURL"}
+  res.render("urls_show", templateVars);
+});
+
 
 
 app.listen(PORT, () => {

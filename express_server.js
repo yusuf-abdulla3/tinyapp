64cc0877fbd47c28c5de869021e273ed7d3808d8
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-08-03 11:17:36 +0000

    installed the body parser library and used it to make the POST request readable. Also created a function that returns a string of 6 random alphanumeric characters that will be used for the short URL.

--- express_server.js
+++ express_server.js
@@ -2,12 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
 
+const generateRandomString = () => {
+  return Math.random().toString(36).slice(2,8)  
+}
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -22,10 +34,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[`${req.params.shortURL}`]}
   res.render("urls_show", templateVars);
 });
+
 
 
 

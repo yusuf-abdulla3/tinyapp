502802d91e762c6ae809371897f5d0c6349f8b53
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-08-03 03:26:42 +0000

    added an additional endpoint called '/urls' that passes on the urlDatabase data to the urls_index.ejs file

--- express_server.js
+++ express_server.js
@@ -12,12 +12,22 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+
+// app.get("/hello", (req, res) => {
+//   const templateVars = { greeting: "Hello World!" };
+//   res.render("hello_world", templateVars);  
+// });
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
